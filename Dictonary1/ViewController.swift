bf555a2ae5d24ae9753911f824463732e7146c06
Nagapranaya Vemula <nagapranaya.vemula@gmail.com>
2024-08-13 13:21:16 +0530

    first commit

---
+++ Dictonary1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dictonary1
+//
+//  Created by Pranaya on 8/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
