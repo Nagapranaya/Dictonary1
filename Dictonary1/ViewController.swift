27ee2097835a993231ac4e2e634738e7d5f51c54
Nagapranaya Vemula <nagapranaya.vemula@gmail.com>
2024-08-14 12:16:24 +0530

    Final code

--- Dictonary1/ViewController.swift
+++ Dictonary1/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.wordTextField.text = "autism"
+        
         // Do any additional setup after loading the view.
     }
 
@@ -50,6 +50,21 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        print(self.words)
+        print(self.words[indexPath.row])
+        fetchMeaning(completionHandler: {
+            dictonaryArray in
+            print(dictonaryArray)
+            let x = dictonaryArray[0].meanings[0].definitions[0]
+            print(x.definition)
+            DispatchQueue.main.async {
+                self.outputLabel.text = x.definition
+            }
+            
+        }, word:self.words[indexPath.row])
+    }
 
     
 }
@@ -70,9 +85,9 @@ func fetchMeaning(completionHandler: @escaping ([Dictonary])-> Void , word: Stri
         {
             print(String(data: data , encoding: .utf8)!)
             do {
-                let dictonary = try JSONDecoder().decode([Dictonary].self, from: data)
-                print(dictonary)
-                completionHandler(dictonary)
+                let dictonaryArray = try JSONDecoder().decode([Dictonary].self, from: data)
+                print(dictonaryArray)
+                completionHandler(dictonaryArray)
 
             } catch let jsonErr {
                 print("Error printing json data", jsonErr)
@@ -85,4 +100,36 @@ func fetchMeaning(completionHandler: @escaping ([Dictonary])-> Void , word: Stri
    
 }
 
+
+func fetchMeaning2(completionHandler: @escaping ([Dictonary])-> Void , word: String) {
+    let url = URL(string: "https://api.dictionaryapi.dev/api/v2/entries/en/" + word)!
+    let task = URLSession.shared.dataTask(with: url , completionHandler: { (data, response, error) in
+        if let error = error{
+            print("error fetching meaning: \(error)")
+            return
+        }
+        guard let httpResponse = response as?
+                HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else{
+            print("error in response: \(String(describing: response))")
+            return
+        }
+        if let data = data
+            //,let dictonary = try?JSONDecoder().decode([Dictonary].self, from: data)
+        {
+            print(String(data: data , encoding: .utf8)!)
+            do {
+                let dictonaryArray = try JSONDecoder().decode([Dictonary].self, from: data)
+                print(dictonaryArray)
+                completionHandler(dictonaryArray)
+
+            } catch let jsonErr {
+                print("Error printing json data", jsonErr)
+            }
+            
+        }
+        
+    })
+    task.resume()
+   
+}
 
